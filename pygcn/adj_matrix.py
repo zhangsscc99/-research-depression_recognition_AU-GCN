da41a2dc95b11c87fa75e62c791214cf30804ada
zhangsscc99 <yw4554@nyu.edu>
2023-01-25 21:37:11 +0800

    update

--- pygcn/adj_matrix.py
+++ pygcn/adj_matrix.py
@@ -1,14 +1,7 @@
-from train_gcn_test import AU_set_lst
+
 
 #10 sets of AU_data: 0,1,1,1,....
-0,1,1,1,....
-0,1,1,1,....
-.....
 
-total_count=10
-AU1_count=6
-AU2_count=4
-AU_joint=2
 
 def adj_matrix(AU_inc1,AU_inc2,feature):
     cnt1=0
@@ -19,11 +12,11 @@ def adj_matrix(AU_inc1,AU_inc2,feature):
     
 
     for i in range(len(feature)):
-        if  feature[i][AU_inc1]=="1.0":
+        if  feature[i][AU_inc1]==1.0:
             cnt1+=1
-        if  feature[i][AU_inc2]=="1.0":
+        if  feature[i][AU_inc2]==1.0:
             cnt2+=1
-        if  feature[i][AU_inc2]=="1.0" and feature[i][AU_inc1]=='1.0':
+        if  feature[i][AU_inc2]==1.0 and feature[i][AU_inc1]==1.0:
             cnt_joint+=1
     
 
@@ -34,22 +27,31 @@ def adj_matrix(AU_inc1,AU_inc2,feature):
     P_AU1_AU2 = AU1_AU2_joint_count / total_count
     P_AU2 = AU2_count / total_count
     #P_AU1_given_AU2 = P_AU1_AU2 / P_AU2
+    if AU2_count==0.0:
+        return 0.0
     P12=AU1_AU2_joint_count/AU2_count
     return P12
+"""
+index_pair=[]
+for i in range(9):
+    for j in range(9):
+        index_pair.append([i,j])
+print(index_pair,len(index_pair))
+"""
 def get_all():
-    adj_matrix(1,2,AU_set_lst)
-    adj_matrix(2,1,AU_set_lst)
-    adj_matrix(1,4,AU_set_lst)
-    adj_matrix(4,1,AU_set_lst)
-    adj_matrix(2,4,AU_set_lst)
-    adj_matrix(4,2,AU_set_lst)
+    res=[]
 
-    adj_matrix(10,12,AU_set_lst)
-    adj_matrix(12,10,AU_set_lst)
-    adj_matrix(12,14,AU_set_lst)
-    adj_matrix(14,12,AU_set_lst)
-    adj_matrix(10,14,AU_set_lst)
-    adj_matrix(14,10,AU_set_lst)
+    for i in range(9):
+        for j in range(9):
+            res.append(adj_matrix(i,j,AU_set_lst))
+    import numpy as np                  #导入numpy模块，并重命名为np
+    x = np.array(res)     #x是一维数组 
+    d = x.reshape((9,9))                #将x重塑为2行4列的二维数组
+    return d
+
+             
+    
+    
 # ........
 
 
--- pygcn/train_gcn_test.py
+++ pygcn/train_gcn_test.py
@@ -1,6 +1,7 @@
 from __future__ import division
 from __future__ import print_function
 
+
 import time
 import argparse
 import numpy as np
@@ -8,6 +9,9 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
+
+#from pygcn.adj_matrix import get_all
+#from pygcn.adj_matrix import adj_matrix
 
 from pygcn.utils import load_data, accuracy
 from pygcn.models import GCN
@@ -101,6 +105,61 @@ adj3=[]
 for i in range(9):
     adj3.append(adj2)
 adj=torch.FloatTensor(adj3)
+
+
+#10 sets of AU_data: 0,1,1,1,....
+
+
+def adj_matrix(AU_inc1,AU_inc2,feature):
+    cnt1=0
+    cnt2=0
+    cnt_joint=0
+
+    
+    
+
+    for i in range(len(feature)):
+        if  feature[i][AU_inc1]==1.0:
+            cnt1+=1
+        if  feature[i][AU_inc2]==1.0:
+            cnt2+=1
+        if  feature[i][AU_inc2]==1.0 and feature[i][AU_inc1]==1.0:
+            cnt_joint+=1
+    
+
+    AU1_AU2_joint_count = cnt_joint # Number of instances where both AU1 and AU4 are present
+    AU2_count = cnt2 # Number of instances where AU4 is present
+    total_count = len(feature) # Total number of instances in the dataset
+
+    P_AU1_AU2 = AU1_AU2_joint_count / total_count
+    P_AU2 = AU2_count / total_count
+    #P_AU1_given_AU2 = P_AU1_AU2 / P_AU2
+    if AU2_count==0.0:
+        return 0.0
+    P12=AU1_AU2_joint_count/AU2_count
+    return P12
+"""
+index_pair=[]
+for i in range(9):
+    for j in range(9):
+        index_pair.append([i,j])
+print(index_pair,len(index_pair))
+"""
+def get_all():
+    res=[]
+
+    for i in range(9):
+        for j in range(9):
+            res.append(adj_matrix(i,j,AU_set_lst))
+    import numpy as np                  #导入numpy模块，并重命名为np
+    x = np.array(res)     #x是一维数组 
+    d = x.reshape((9,9))                #将x重塑为2行4列的二维数组
+    return d
+
+adj=get_all()
+
+adj=torch.FloatTensor(adj)
+print(adj)
 
 
 
